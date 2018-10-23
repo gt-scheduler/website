d046942a9cbd81cf13c43a2175bcbb81d046f095
Jason Park <parkjs814@gmail.com>
2018-10-23 12:22:23 -0400

    Updates

--- precache-manifest.360056a7bc12eaa0ac88672c7f65e393.js
+++ precache-manifest.360056a7bc12eaa0ac88672c7f65e393.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/gt-scheduler/static/js/runtime~main.3e8e58b9.js"
   },
   {
-    "revision": "88a8f6e48f9402e735df",
-    "url": "/gt-scheduler/static/js/main.88a8f6e4.chunk.js"
+    "revision": "bd5934f7524ac293e559",
+    "url": "/gt-scheduler/static/js/main.bd5934f7.chunk.js"
   },
   {
     "revision": "e514213d74e05990cedb",
     "url": "/gt-scheduler/static/js/1.e514213d.chunk.js"
   },
   {
-    "revision": "88a8f6e48f9402e735df",
-    "url": "/gt-scheduler/static/css/main.dea6fd3b.chunk.css"
+    "revision": "bd5934f7524ac293e559",
+    "url": "/gt-scheduler/static/css/main.bfaef06c.chunk.css"
   },
   {
-    "revision": "336053213ee9cdd7a2c347dee744cffe",
+    "revision": "8ffb70774dc59ae4ab579f64a3b48fbd",
     "url": "/gt-scheduler/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/gt-scheduler/precache-manifest.da20b4d164bf0bd43ff7447bc5ca7a24.js"
+  "/gt-scheduler/precache-manifest.360056a7bc12eaa0ac88672c7f65e393.js"
 );
 
 workbox.clientsClaim();
