59e693db1022334d8384e0014357684b85f2b698
64json <parkjs814@gmail.com>
2020-07-20 05:23:00 +0000

    Deploying to gh-pages from  @ 371e1f1024b60b43b6340408250260393d9fea3f 🚀

---
+++ precache-manifest.a1ca2c8e62253736e29d6368e28a48bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63770694770631facaab374425eaca8b",
+    "url": "/gt-scheduler/index.html"
+  },
+  {
+    "revision": "5ee0fc9c52bdd18ff3e2",
+    "url": "/gt-scheduler/static/css/2.771d4c7b.chunk.css"
+  },
+  {
+    "revision": "dfe6955cbd87586d82c2",
+    "url": "/gt-scheduler/static/css/main.7cf7949f.chunk.css"
+  },
+  {
+    "revision": "5ee0fc9c52bdd18ff3e2",
+    "url": "/gt-scheduler/static/js/2.913b6280.chunk.js"
+  },
+  {
+    "revision": "c51656bd4496813f03872ab74def5856",
+    "url": "/gt-scheduler/static/js/2.913b6280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe6955cbd87586d82c2",
+    "url": "/gt-scheduler/static/js/main.150deb0b.chunk.js"
+  },
+  {
+    "revision": "dbccc84375c4a1a210b3",
+    "url": "/gt-scheduler/static/js/runtime-main.705b6edb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gt-scheduler/precache-manifest.2a0e8e3896a8577032ed4654fc4b1e9e.js"
+  "/gt-scheduler/precache-manifest.a1ca2c8e62253736e29d6368e28a48bc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gt-scheduler/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gt-scheduler/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
