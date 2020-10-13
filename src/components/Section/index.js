5c4052f2c7082da62f868321c12bed1e47770efe
Nicholas Boshart <nichabosh.code@gmail.com>
2020-10-13 19:14:37 -0400

    Sequential Seating Integration & Display
    • Created fetchSeating(...) function in src/beans/Section.js.
       -> Appends necessary seating info to the section class.
       -> Only fetches if information hasn't been retrieved in >5 mins.
       -> Scraper created by @sauravghosal!
    • Implemented tooltip display using react-tooltip dependency.
       -> Appended necessary attributes to chair action in Section/index.js.
       -> Attached these additions to ActionRow component in ActionRow/index.js.
    • Added seating information to imported <ReactToolTip> component.
       -> Relevant tooltip information is retrieved upon hovering over chair icon.
       -> User must be hovered for at least 0.333 seconds before info is fetched.

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
+import ReactTooltip from "react-tooltip";
 import {
   faBan,
   faChair,
@@ -16,6 +17,16 @@ export function Section({ className, section, pinned, color }) {
     TermContext
   );
   const [, setOverlayCrns] = useContext(OverlayCrnsContext);
+  const [seating, setSeating] = useState([[], []]);
+
+  let hovering = false;
+  const handleHover = () => {
+    hovering = true;
+    setTimeout(async () => {
+      if (hovering)
+        setSeating(await section.fetchSeating(term));
+    }, 333);
+  };
 
   const excludeSection = useCallback(
     (section) => {
@@ -56,6 +67,10 @@ export function Section({ className, section, pinned, color }) {
         },
         {
           icon: faChair,
+          dataTip: true,
+          dataFor: section.id,
+          onMouseEnter: () => { handleHover() },
+          onMouseLeave: () => { hovering = false },
           href: `https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_detail_sched?term_in=${term}&crn_in=${section.crn}`
         },
         { icon: faBan, onClick: () => excludeSection(section) }
@@ -76,6 +91,34 @@ export function Section({ className, section, pinned, color }) {
             );
           })}
         </div>
+
+        <ReactTooltip
+          id={section.id}
+          type="dark"
+          place="right" 
+          effect="solid"
+          className="tooltip"
+        >
+            <table>
+              <tbody>
+                <tr>
+                  <td><b>Seats Open</b></td>
+                  <td>{seating[0].length === 0 ? `Loading...` :
+                    typeof seating[0][1] === "number" ?
+                      `${seating[0][0] - seating[0][1]} / ${seating[0][0]}` : `N/A`
+                  }</td>
+                </tr>
+                <tr>
+                  <td><b>Waitlist Open</b></td>
+                  <td>{
+                    seating[0].length === 0 ? `Loading...` :
+                      typeof seating[0][1] === "number" ?
+                        `${seating[0][2] - seating[0][3]} / ${seating[0][2]}` : `N/A`
+                  }</td>
+                </tr>
+              </tbody>
+            </table>
+        </ReactTooltip>
       </div>
     </ActionRow>
   );
