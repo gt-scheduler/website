076714c1544fd5c77ca1d1923a41a7e7b79c5ce9
Joseph Azevedo <joseph.az@gatech.edu>
2020-11-02 21:14:08 -0500

    feat: add basic TypeScript support; add root types

--- src/contexts/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const ThemeContext = React.createContext(null);
-export const TermsContext = React.createContext(null);
-export const TermContext = React.createContext(null);
-export const OverlayCrnsContext = React.createContext(null);
