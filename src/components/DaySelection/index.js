5b28b8cf9fa1343de434b39c59e60c1938bf8ef8
sauravghosal <sghosal9@gatech.edu>
2020-11-04 15:11:31 -0500

    styling

--- src/components/DaySelection/index.js
+++ src/components/DaySelection/index.js
@@ -1,31 +1,79 @@
 import React, { useState } from 'react';
+import { ActionRow } from '..';
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { classes, getContentClassName } from '../../utils';
+import './stylesheet.scss';
 
 export function DaySelection({ courseDateMap }) {
   const [activeDay, setActiveDay] = useState('');
+  const colorPalette = ['#FCB9AA', '#FFDBCC', '#ECEAE4', '#A2E1DB', '#55CBCD'];
+  const daysOfTheWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+  ];
+
+  function LightenDarkenColor(col, amt) {
+    let num = parseInt(col, 16);
+    let r = (num >> 16) + amt;
+    let b = ((num >> 8) & 0x00ff) + amt;
+    let g = (num & 0x0000ff) + amt;
+    let newColor = g | (b << 8) | (r << 16);
+    return newColor.toString(16);
+  }
 
   const formatTime = (time) => {
-    return (Math.floor(time / 60) % 12) + ':' + (time % 60);
+    return (
+      (Math.floor(time / 60) > 12
+        ? Math.floor(time / 60) % 12
+        : Math.floor(time / 60)) +
+      ':' +
+      (time % 60 == 0 ? '00' : time % 60)
+    );
   };
 
-  // active course?
-  // add a new div for each day of the week
   return (
-    <div>
-      {Object.keys(courseDateMap).map((date) => (
-        <div className={`${date}-block`}>
-          <div onClick={() => setActiveDay(date)}>{date}</div>
-          <ul hidden={date !== activeDay ? true : false}>
-            {courseDateMap[date].map((course) => (
-              <li>
-                <p className="course-id">{course.id}</p>
-                <p className="course-title">{course.title}</p>
-                <p className="course-times">
-                  {formatTime(course.times.start)} -
-                  {formatTime(course.times.end)}
-                </p>
-              </li>
-            ))}
-          </ul>
+    <div className="date-container">
+      {Object.keys(courseDateMap).map((date, i) => (
+        <div
+          className={classes(
+            'date',
+            getContentClassName(colorPalette[i]),
+            'default'
+          )}
+          style={{ backgroundColor: colorPalette[i] }}
+        >
+          <ActionRow
+            label={daysOfTheWeek[i]}
+            className="day-select"
+            actions={[
+              {
+                icon: date === activeDay ? faAngleUp : faAngleDown,
+                onClick: () =>
+                  date !== activeDay ? setActiveDay(date) : setActiveDay('')
+              }
+            ]}
+          />
+          {activeDay === date && (
+            <div className="dropdown-content">
+              {courseDateMap[date].length === 0 ? (
+                <div className="course-content">No classes this day!</div>
+              ) : (
+                courseDateMap[date].map((course, i) => (
+                  <div className="course-content">
+                    <div className="course-id">{course.id}</div>
+                    <span className="course-row">{course.title}</span>
+                    <span className="course-row">
+                      {formatTime(course.times.start)} -
+                      {formatTime(course.times.end)}
+                    </span>
+                  </div>
+                ))
+              )}
+            </div>
+          )}
         </div>
       ))}
     </div>
