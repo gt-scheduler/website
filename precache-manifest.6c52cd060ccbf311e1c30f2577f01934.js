58f99cbfffa18e632abe8e4a773a29bcf5d3294b
nichabosh <70110771+nichabosh@users.noreply.github.com>
2020-11-11 07:33:44 +0000

    Deploying to gh-pages from  @ c1d8b6f72f0edad7673c63c6f84c61009d8104c2 🚀

---
+++ precache-manifest.6c52cd060ccbf311e1c30f2577f01934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abda387a0ade2fe5d71e6cc3073b40cd",
+    "url": "/gt-scheduler/index.html"
+  },
+  {
+    "revision": "3cf4b03dd2b04aec32f6",
+    "url": "/gt-scheduler/static/css/2.771d4c7b.chunk.css"
+  },
+  {
+    "revision": "e1a447508dde0effd091",
+    "url": "/gt-scheduler/static/css/main.f3c93f35.chunk.css"
+  },
+  {
+    "revision": "3cf4b03dd2b04aec32f6",
+    "url": "/gt-scheduler/static/js/2.b0132d82.chunk.js"
+  },
+  {
+    "revision": "cba1e91a6e64c5f75570975afbbe37a3",
+    "url": "/gt-scheduler/static/js/2.b0132d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a447508dde0effd091",
+    "url": "/gt-scheduler/static/js/main.87b6b628.chunk.js"
+  },
+  {
+    "revision": "dbccc84375c4a1a210b3",
+    "url": "/gt-scheduler/static/js/runtime-main.705b6edb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d033650c1dadde879e84f521f01a066.js"
+  "/gt-scheduler/precache-manifest.6c52cd060ccbf311e1c30f2577f01934.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gt-scheduler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
