d17bfa4580cba41b1c61b74574baa56987a03a8c
Joseph Azevedo <joseph.az@gatech.edu>
2021-02-18 17:51:24 -0500

    feat: migrate from GTBitsOfGood/gt-scheduler#26
    Co-authored-by: sauravghosal <sghosal9@gatech.edu>

--- src/components/ActionRow/index.tsx
+++ src/components/ActionRow/index.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { classes } from '../../utils';
+import Button, { ButtonProps } from '../Button';
+
 import './stylesheet.scss';
-import { Button } from '..';
+
+type FontAwesomeProps = React.ComponentProps<typeof FontAwesomeIcon>;
+export type Action = {
+  icon: FontAwesomeProps['icon'];
+  styling?: React.CSSProperties;
+  dataTip?: string;
+  dataFor?: string;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+} & Omit<ButtonProps, 'children'>;
+
+type BaseActionRowProps = {
+  label: string;
+  actions: Action[];
+  className?: string;
+  children?: React.ReactNode;
+};
+export type ActionRowProps = BaseActionRowProps &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof BaseActionRowProps>;
 
 export default function ActionRow({
   className,
   label,
   children,
   actions,
-  ...props
-}) {
+  ...restProps
+}: ActionRowProps) {
   return (
-    <div className={classes('ActionRow', className)} {...props}>
+    <div className={classes('ActionRow', className)} {...restProps}>
       <div className="action-row-header">
         <div className="label">{label}</div>
         <div className={classes('actions', 'default')}>
@@ -22,22 +42,20 @@ export default function ActionRow({
               (
                 {
                   icon,
-                  title,
                   styling,
                   dataTip,
                   dataFor,
                   onMouseEnter,
                   onMouseLeave,
-                  ...restProps
+                  ...rest
                 },
                 i
               ) => (
-                <Button className="action" {...restProps} key={i}>
+                <Button className="action" {...rest} key={i}>
                   <FontAwesomeIcon
                     fixedWidth
                     style={styling}
                     icon={icon}
-                    title={title}
                     data-tip={dataTip}
                     data-for={dataFor}
                     onMouseEnter={onMouseEnter}
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,15 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MapView from '../MapView';
 import { periodToString } from '../../utils';
 import { TermContext } from '../../contexts';
+import DaySelection from '../DaySelection';
 
 const Map = () => {
   const [{ oscar, pinnedCrns }] = useContext(TermContext);
+  const [activeDay, setActiveDay] = useState('');
   const locations = [];
+  const courseDateMap = {
+    M: [],
+    T: [],
+    W: [],
+    R: [],
+    F: []
+  };
 
+  // Construct the courseDateMap and locations data structures
   pinnedCrns.forEach((crn) => {
     const info = oscar.crnMap[crn.toString()];
     const meetings = info.meetings[0];
+
+    meetings.days.forEach((day) => {
+      courseDateMap[day].push({
+        id: info.course.id,
+        title: info.course.title,
+        times: meetings.period,
+        daysOfWeek: meetings.days
+      });
+    });
 
     locations.push({
       section: info.id,
@@ -21,7 +40,23 @@ const Map = () => {
     });
   });
 
-  return <MapView locations={locations} />;
+  let activeLocations = [];
+  if (activeDay !== '') {
+    activeLocations = locations.filter((loc) =>
+      courseDateMap[activeDay].some((course) => course.id === loc.id)
+    );
+  }
+
+  return (
+    <div className="map-content" style={{ display: 'flex', height: '100vh' }}>
+      <DaySelection
+        courseDateMap={courseDateMap}
+        activeDay={activeDay}
+        setActiveDay={setActiveDay}
+      />
+      <MapView locations={activeLocations} />
+    </div>
+  );
 };
 
 export default Map;
