b4378115c17a1bc4d5404ca92da4def40ea2a717
Joseph Azevedo <joseph.az@gatech.edu>
2021-03-21 16:38:42 -0400

    feat: make time logic easier to follow

--- src/components/DaySelection/index.tsx
+++ src/components/DaySelection/index.tsx
@@ -28,11 +28,15 @@ export default function DaySelection({
 
   const formatTime = (time: number): string => {
     if (Math.floor(time / 60) >= 12) {
-      return `${
-        Math.floor(time / 60) % 12 === 0 ? '12' : Math.floor(time / 60) % 12
-      }:${time % 60 === 0 ? '00' : time % 60}pm`;
+      const adjustedHour = Math.floor(time / 60) % 12;
+      const hour = adjustedHour === 0 ? '12' : adjustedHour;
+      const minute = String(time % 60).padStart(2, '0');
+      return `${hour}:${minute}pm`;
     }
-    return `${Math.floor(time / 60)}:${time % 60 === 0 ? '00' : time % 60}am`;
+
+    const hour = Math.floor(time / 60);
+    const minute = String(time % 60).padStart(2, '0');
+    return `${hour}:${minute}am`;
   };
 
   return (
