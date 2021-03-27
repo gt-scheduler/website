c4efd2ed4087ac05dc6d90de318032ef2d96c5b1
Joseph Azevedo <joseph.az@gatech.edu>
2021-03-27 19:37:25 -0400

    feat: disable submit while loading; refactor button gen

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -12,12 +12,21 @@ export default function Feedback() {
   const [submit, setSubmit] = useState(false);
   const [rating, setRating] = useState(null);
   const [feedback, setFeedback] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = () =>
+  const onSubmit = () => {
+    setLoading(true);
     // Add 1 to the rating to move it from [0,4] to [1,5]
     FormSubmit({ rating: rating + 1, feedback })
-      .then(() => setSubmit(true))
-      .catch(() => setSubmit(true));
+      .then(() => {
+        setSubmit(true);
+        setLoading(false);
+      })
+      .catch(() => {
+        setSubmit(true);
+        setLoading(false);
+      });
+  };
 
   return (
     <div>
@@ -44,51 +53,17 @@ export default function Feedback() {
                 <div>
                   <p className="text">How would you rate your experience?</p>
                   <div className="FormButtons">
-                    <Button
-                      className={classes(
-                        'FormButton',
-                        rating === 0 && 'active'
-                      )}
-                      onClick={() => setRating(0)}
-                    >
-                      1
-                    </Button>
-                    <Button
-                      className={classes(
-                        'FormButton',
-                        rating === 1 && 'active'
-                      )}
-                      onClick={() => setRating(1)}
-                    >
-                      2
-                    </Button>
-                    <Button
-                      className={classes(
-                        'FormButton',
-                        rating === 2 && 'active'
-                      )}
-                      onClick={() => setRating(2)}
-                    >
-                      3
-                    </Button>
-                    <Button
-                      className={classes(
-                        'FormButton',
-                        rating === 3 && 'active'
-                      )}
-                      onClick={() => setRating(3)}
-                    >
-                      4
-                    </Button>
-                    <Button
-                      className={classes(
-                        'FormButton',
-                        rating === 4 && 'active'
-                      )}
-                      onClick={() => setRating(4)}
-                    >
-                      5
-                    </Button>
+                    {[1, 2, 3, 4, 5].map((i) => (
+                      <Button
+                        className={classes(
+                          'FormButton',
+                          rating === i - 1 && 'active'
+                        )}
+                        onClick={() => setRating(i - 1)}
+                      >
+                        {i}
+                      </Button>
+                    ))}
                   </div>
                   <div className="ScoreLabels">
                     <span className="score" style={{ marginLeft: '4px' }}>
@@ -107,7 +82,7 @@ export default function Feedback() {
                   <Button
                     className="SubmitButton"
                     onClick={onSubmit}
-                    disabled={rating == null}
+                    disabled={rating == null || loading}
                   >
                     Submit
                   </Button>
