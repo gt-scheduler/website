0a222d2c816c66bb2bffa4181a30ac18149bd930
Joseph Azevedo <joseph.az@gatech.edu>
2021-08-29 17:25:17 -0400

    Better utilize TS in Prerequisite component to separate component variants (#52)

--- src/components/Prerequisite/index.tsx
+++ src/components/Prerequisite/index.tsx
@@ -6,82 +6,234 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 import { TermContext } from '../../contexts';
-import { classes, decryptReqs } from '../../utils';
+import { classes, serializePrereqs } from '../../utils';
 import { ActionRow } from '..';
 import { Course } from '../../beans';
-import { PrerequisiteClause } from '../../types';
+import {
+  CrawlerPrerequisites,
+  PrerequisiteClause,
+  PrerequisiteOperator,
+} from '../../types';
+import { ErrorWithFields, softError } from '../../log';
+
+const BASE_ITEM_STYLE = { fontSize: '0.9em', padding: '8px' };
 
 export type PrerequisiteProps = {
-  course?: Course;
-  option?: number;
-  req?: PrerequisiteClause;
-  isHeader?: boolean;
-  isEmpty?: boolean;
-  isLast?: boolean;
+  course: Course;
+  prereqs: CrawlerPrerequisites;
 };
 
+/**
+ * Renders the prereqs for a single course, given that the crawler version
+ * supports prereqs. (Make sure to check this before). An empty prereq list is
+ * an authoritative statement that the course has no prereqs, and as such a
+ * message will be displayed telling the user. Otherwise, this component tries
+ * to render the prereq tree in a way that is easily consumable without taking
+ * up too much screen space, first trying to split the prereqs up into separate
+ * "options" (sub-clauses of an OR set) if possible before rendering a list of
+ * items with operators in between. Finally, in each item, there is the
+ * flattened textual representation of the remainder of the subtree, which
+ * restores parentheses groupings (much like the original Oscar prereq syntax)
+ */
 export default function Prerequisite({
   course,
-  req,
-  option,
-  isHeader = false,
-  isEmpty = false,
-  isLast = false,
+  prereqs,
 }: PrerequisiteProps): React.ReactElement {
-  const [{ term }] = useContext(TermContext);
-  const [expanded, setExpanded] = useState(true);
-  const reqStyle = { fontSize: '0.9em', padding: '8px' };
+  let content: React.ReactNode;
+  if (prereqs.length === 0) {
+    content = <PrerequisiteEmpty />;
+  } else {
+    // `prereqs` isn't an empty array, so it must be `PrerequisiteSet`
+    // (here we manually `Exclude` the empty array case)
+    const [op, ...subClauses] = prereqs as Exclude<CrawlerPrerequisites, []>;
 
-  const subreqs: PrerequisiteClause[] | null =
-    typeof req === 'undefined'
-      ? []
-      : !Array.isArray(req)
-      ? [req]
-      : typeof req[0] !== 'object'
-      ? (req.slice(1, req.length) as PrerequisiteClause[])
-      : typeof req[0] === 'object'
-      ? [req]
-      : null;
+    switch (op) {
+      case 'and':
+        // We only consider this a single "option",
+        // so just render the content as a direct child
+        content = <PrerequisiteClauseDisplay clause={['and', ...subClauses]} />;
+        break;
+      case 'or':
+        if (subClauses.length === 1) {
+          // There is only 1 option:
+          // just render the content as a direct child
+          content = (
+            <PrerequisiteClauseDisplay
+              clause={subClauses[0] as PrerequisiteClause}
+            />
+          );
+        } else {
+          // Render an option for each sub-clause
+          content = (
+            <>
+              {subClauses.map((subClause, i) => (
+                <PrerequisiteOption key={i} clause={subClause} index={i} />
+              ))}
+            </>
+          );
+        }
+        break;
+      default:
+        softError(
+          new ErrorWithFields({
+            message: 'invalid operator found in top-level prereqs',
+            fields: {
+              courseId: course.id,
+              operator: op,
+            },
+          })
+        );
+        content = null;
+    }
+  }
 
   return (
-    <div>
-      {!isHeader && !isEmpty && req && (
-        <div style={reqStyle}>
-          {decryptReqs(req)} {!isLast && <strong>and</strong>}
+    <div className={classes('hover-container', 'nested')}>
+      <PrerequisiteHeader course={course} />
+      <div className={classes('nested')}>{content}</div>
+    </div>
+  );
+}
+
+// Private sub-components
+
+type PrerequisiteHeaderProps = {
+  course: Course;
+};
+
+/**
+ * Renders the "header" at the top of a prereq display,
+ * which includes a link to Oscar giving the original prereq source,
+ * plus other course information.
+ */
+function PrerequisiteHeader({
+  course,
+}: PrerequisiteHeaderProps): React.ReactElement {
+  const [{ term }] = useContext(TermContext);
+
+  return (
+    <ActionRow
+      className={classes('hover-container')}
+      label="Prerequisites"
+      actions={[
+        {
+          icon: faInfoCircle,
+          href:
+            `https://oscar.gatech.edu/pls/bprod/bwckctlg.p_disp_` +
+            `course_detail?cat_term_in=${term}&subj_code_in=` +
+            `${course.subject}&crse_numb_in=${course.number}`,
+        },
+      ]}
+    />
+  );
+}
+
+type PrerequisiteOptionProps = {
+  clause: PrerequisiteClause;
+  index: number;
+};
+
+/**
+ * Renders a single prereq option -- a sub-clause of a larger "OR" prereq set
+ * that is independently collapse-able from the other sibling options.
+ * Inside, uses `PrerequisiteClauseDisplay` to render the sub-clause if
+ * the option is expanded.
+ */
+function PrerequisiteOption({
+  clause,
+  index,
+}: PrerequisiteOptionProps): React.ReactElement {
+  const [expanded, setExpanded] = useState(true);
+
+  return (
+    <>
+      <ActionRow
+        className={classes('hover-container')}
+        label={`Option ${index + 1}`}
+        actions={[
+          {
+            icon: expanded ? faAngleUp : faAngleDown,
+            onClick: (): void => setExpanded(!expanded),
+          },
+        ]}
+      />
+      {expanded && (
+        <div className={classes('nested')}>
+          <PrerequisiteClauseDisplay clause={clause} />
         </div>
       )}
-      {isHeader && course != null && (
-        <ActionRow
-          className={classes('hover-container')}
-          label={req ? `Option ${option ?? '?'}` : `Prerequisites`}
-          actions={[
-            !req
-              ? {
-                  icon: faInfoCircle,
-                  href:
-                    `https://oscar.gatech.edu/pls/bprod/bwckctlg.p_disp_` +
-                    `course_detail?cat_term_in=${term}&subj_code_in=` +
-                    `${course.subject}&crse_numb_in=${course.number}`,
-                }
-              : null,
-            req && {
-              icon: expanded ? faAngleUp : faAngleDown,
-              onClick: (): void => setExpanded(!expanded),
-            },
-          ]}
+    </>
+  );
+}
+
+type PrerequisiteClauseDisplayProps = {
+  clause: PrerequisiteClause;
+};
+
+/**
+ * Renders an arbitrary prereq clause. If the clause is a singular course,
+ * then it renders a single Item. Otherwise, if the clause is a set,
+ * this component renders an item for each member of the set, and includes
+ * an operator at the end of each item's text to indicate that each item
+ * is part of a larger prereq set.
+ */
+function PrerequisiteClauseDisplay({
+  clause,
+}: PrerequisiteClauseDisplayProps): React.ReactElement {
+  if (!Array.isArray(clause)) {
+    // Render the single prereq course
+    return <PrerequisiteItem clause={clause} operator="and" isLast />;
+  }
+
+  // Render a list of prereq items
+  const [operator, ...subClauses] = clause;
+  return (
+    <>
+      {subClauses.map((subClause, i) => (
+        <PrerequisiteItem
+          key={i}
+          clause={subClause}
+          operator={operator}
+          isLast={i === subClauses.length - 1}
         />
-      )}
-      {isEmpty && (
-        <div style={reqStyle}>No prerequisites. You&apos;re good to go!</div>
-      )}
-      {expanded &&
-        isHeader &&
-        subreqs &&
-        subreqs.map((reqs, i) => (
-          <div key={i} className={classes('divider-bottom', 'nested')}>
-            <Prerequisite req={reqs} isLast={i === subreqs.length - 1} />
-          </div>
-        ))}
+      ))}
+    </>
+  );
+}
+
+type PrerequisiteItemProps = {
+  clause: PrerequisiteClause;
+  operator: PrerequisiteOperator;
+  isLast: boolean;
+};
+
+/**
+ * Renders a single "item" -- a div with the completely flattened text
+ * representation of the prereq subtree passed in as `clause` (whether
+ * that's a single prereq course or a sprawling sub-tree with many branches).
+ * Includes the ability to display a higher-level operator between different
+ * `PrerequisiteItem`'s as long as `isLast` is false, which is used by
+ * `PrerequisiteClauseDisplay` when it needs to render a prerequisite set.
+ `
+ */
+function PrerequisiteItem({
+  clause,
+  operator,
+  isLast,
+}: PrerequisiteItemProps): React.ReactElement {
+  return (
+    <div className={classes('divider-bottom')} style={BASE_ITEM_STYLE}>
+      {serializePrereqs(clause)} {!isLast && <strong>{operator}</strong>}
     </div>
+  );
+}
+
+/**
+ * Replacement prerequisite "item"-like component that simply contains
+ * a notification to the user that the course has no prereqs.
+ */
+function PrerequisiteEmpty(): React.ReactElement {
+  return (
+    <div style={BASE_ITEM_STYLE}>No prerequisites. You&apos;re good to go!</div>
   );
 }
