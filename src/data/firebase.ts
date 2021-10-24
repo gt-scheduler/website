922aa9fbbf1b5fb48e2199f15c387384dbece2a5
Joseph Azevedo <joseph.az@gatech.edu>
2021-10-23 23:52:38 -0400

    fix console warning by importing firebase component

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 import { ErrorWithFields, softError } from '../log';
 
