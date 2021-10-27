ff911cb9a89c10c38beeac046e885a2273f4fd66
Joseph Azevedo <joseph.az@gatech.edu>
2021-10-27 01:59:51 -0400

    add CI config to inject firebase config

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -14,6 +14,7 @@ const firebaseConfig = {
   storageBucket: process.env['REACT_APP_FIREBASE_STORAGE_BUCKET'],
   messagingSenderId: process.env['REACT_APP_FIREBASE_MESSAGING_SENDER_ID'],
   appId: process.env['REACT_APP_FIREBASE_APP_ID'],
+  measurementId: process.env['REACT_APP_FIREBASE_MEASUREMENT_ID'],
 };
 
 /**
