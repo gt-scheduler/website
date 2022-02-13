ce3ef772e9ef109fa639d409edbd48edcb2a085e
luke9kim8 <luke9kim8@gmail.com>
2022-02-13 00:48:26 -0500

    Add course search view for desktop

---
+++ src/components/SearchContainer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './stylesheet.scss';
+
+export default function SearchContainer(): React.ReactElement {
+  return (
+    <div className="SearchContainer">
+      <h3 className="label">Search for a course</h3>
+      <div className="scroller" />
+    </div>
+  );
+}
