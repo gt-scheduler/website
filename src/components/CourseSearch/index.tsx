b16a7e5407006323223f30c4672735b3b06cf652
luke9kim8 <luke9kim8@gmail.com>
2022-02-13 01:02:27 -0500

    Add mobile views

--- src/components/CourseSearch/index.tsx
+++ src/components/CourseSearch/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { DESKTOP_BREAKPOINT } from '../../constants';
+import { classes } from '../../utils/misc';
 import useScreenWidth from '../../hooks/useScreenWidth';
 import CurrentCourseContainer from '../CurrentCourseContainer';
 import SearchContainer from '../SearchContainer';
 import SearchResultContainer from '../SearchResultContainer';
+import { Button, Calendar, CombinationContainer, CourseContainer } from '..';
 
 export default function CourseSearch(): React.ReactElement {
   const mobile = !useScreenWidth(DESKTOP_BREAKPOINT);
@@ -11,6 +13,19 @@ export default function CourseSearch(): React.ReactElement {
 
   return (
     <>
+      {mobile && (
+        <div className="tab-container">
+          {['Current', 'Search', 'Result'].map((tabTitle, i) => (
+            <Button
+              key={tabTitle}
+              className={classes('tab', tabIndex === i && 'active')}
+              onClick={(): void => setTabIndex(i)}
+            >
+              {tabTitle}
+            </Button>
+          ))}
+        </div>
+      )}
       <div className="main">
         {(!mobile || tabIndex === 0) && <CurrentCourseContainer />}
         {(!mobile || tabIndex === 1) && <SearchContainer />}
