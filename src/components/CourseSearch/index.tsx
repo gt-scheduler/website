d848a24f3cf2752eb331c0b686e4615c6b447946
luke9kim8 <luke9kim8@gmail.com>
2022-03-01 22:30:55 -0500

    Add filter for start and end times Fix the bug where 'my classes' won't show up on 2nd col of Course Search tab

--- src/components/CourseSearch/index.tsx
+++ src/components/CourseSearch/index.tsx
@@ -5,22 +5,29 @@ import React, {
   useMemo,
   useRef,
   useState,
+  KeyboardEvent,
 } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-import { classes } from '../../utils/misc';
+import { classes, getRandomColor } from '../../utils/misc';
 import { Course, CourseFilter } from '..';
 import { ScheduleContext } from '../../contexts';
-import { Course as CourseBean } from '../../data/beans';
-import { CAMPUSES, DELIVERY_MODES } from '../../constants';
+import { Course as CourseBean, Section } from '../../data/beans';
+import {
+  ASYNC_DELIVERY_MODE,
+  CAMPUSES,
+  COURSE_CARD_TYPES,
+  DELIVERY_MODES,
+} from '../../constants';
 import CourseFilterByCreditHours from '../CourseFilterByCreditHours';
 
 import 'react-virtualized/styles.css';
 import './stylesheet.scss';
 
-export type CourseAddProps = {
+export type CourseSearchProps = {
   className?: string;
+  onShow: (courseToAdd: CourseBean) => void;
 };
 
 export type SortKey =
@@ -51,6 +58,54 @@ function isSortKey(sortKey: string): sortKey is SortKey {
   }
 }
 
+function doesFilterMatchSection(section: Section, filter: SortFilter): boolean {
+  return Object.entries(filter).every(([key, tags]) => {
+    if (!isSortKey(key)) return true;
+    if (key === 'days')
+      return section.meetings.some((meeting) => {
+        const sortValue = meeting[key];
+        return sortValue.some((day) => tags.includes(day));
+      });
+    if (key === 'credits') {
+      const sortValue = section[key];
+      return tags.includes(`${sortValue}`);
+    }
+
+    if (key === 'startTime') {
+      const startTimeLimitStr = filter.startTime[0];
+      if (startTimeLimitStr) {
+        return section.meetings.some((meeting) => {
+          const startTime = meeting.period?.start;
+          const startTimeLimit = parseInt(startTimeLimitStr, 10);
+          return startTime && startTime >= startTimeLimit;
+        });
+      }
+      return true;
+    }
+
+    if (key === 'endTime') {
+      const endTimeLimitStr = filter.endTime[0];
+      if (endTimeLimitStr) {
+        return section.meetings.some((meeting) => {
+          const endTime = meeting.period?.end;
+          const endTimeLimit = parseInt(endTimeLimitStr, 10);
+          return endTime && endTime <= endTimeLimit;
+        });
+      }
+      return true;
+    }
+
+    // TODO: Implement filter
+    if (key === 'courseLevel') {
+      return true;
+    }
+
+    const sortValue = section[key];
+    if (sortValue == null) return false;
+    return tags.length === 0 || tags.includes(sortValue);
+  });
+}
+
 function doesFilterMatchCourse(
   course: CourseBean,
   filter: SortFilter
@@ -76,13 +131,31 @@ function doesFilterMatchCourse(
         })
       );
 
-    // TODO: Implement filter
     if (key === 'startTime') {
+      const startTimeLimitStr = filter.startTime[0];
+      if (startTimeLimitStr) {
+        return course.sections.some((section) => {
+          return section.meetings.some((meeting) => {
+            const startTime = meeting.period?.start;
+            const startTimeLimit = parseInt(startTimeLimitStr, 10);
+            return startTime && startTime >= startTimeLimit;
+          });
+        });
+      }
       return true;
     }
 
-    // TODO: Implement filter
     if (key === 'endTime') {
+      const endTimeLimitStr = filter.endTime[0];
+      if (endTimeLimitStr) {
+        return course.sections.some((section) => {
+          return section.meetings.some((meeting) => {
+            const endTime = meeting.period?.end;
+            const endTimeLimit = parseInt(endTimeLimitStr, 10);
+            return endTime && endTime <= endTimeLimit;
+          });
+        });
+      }
       return true;
     }
 
@@ -102,11 +175,11 @@ function doesFilterMatchCourse(
   });
 }
 
-export default function CourseSearch(): React.ReactElement {
-  const [{ oscar, desiredCourses }] = useContext(ScheduleContext);
-  // const [{ oscar, desiredCourses,
-  //          excludedCrns, colorMap }, { patchSchedule }] =
-  // useContext(ScheduleContext);
+export default function CourseSearch({
+  onShow,
+}: CourseSearchProps): React.ReactElement {
+  const [{ oscar, desiredCourses, excludedCrns, colorMap }, { patchSchedule }] =
+    useContext(ScheduleContext);
   const [keyword, setKeyword] = useState('');
   const [filter, setFilter] = useState<SortFilter>({
     deliveryMode: [],
@@ -119,7 +192,6 @@ export default function CourseSearch(): React.ReactElement {
   });
   const [activeIndex, setActiveIndex] = useState(0);
   const inputRef = useRef<HTMLInputElement | null>(null);
-  const [selectedCourse, setSelectedCourse] = useState<CourseBean[]>([]);
 
   const handleChangeKeyword = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -147,15 +219,14 @@ export default function CourseSearch(): React.ReactElement {
 
     setActiveIndex(0);
 
-    return oscar.courses
-      .filter((course) => {
-        const keywordMatch =
-          course.subject === subject && course.number.startsWith(number);
-        const filterMatch = doesFilterMatchCourse(course, filter);
-        return keywordMatch && filterMatch;
-      })
-      .filter((course) => !desiredCourses.includes(course.id));
-  }, [oscar, keyword, filter, desiredCourses]);
+    return oscar.courses.filter((course) => {
+      const keywordMatch =
+        course.subject === subject && course.number.startsWith(number);
+      const filterMatch = doesFilterMatchCourse(course, filter);
+      return keywordMatch && filterMatch;
+    });
+    // .filter((course) => !desiredCourses.includes(course.id));
+  }, [oscar, keyword, filter]);
 
   const handleToggleFilter = useCallback(
     (key: SortKey, tag: string) => {
@@ -165,6 +236,16 @@ export default function CourseSearch(): React.ReactElement {
         [key]: tags.includes(tag)
           ? tags.filter((v) => v !== tag)
           : [...tags, tag],
+      });
+    },
+    [filter]
+  );
+
+  const handleToggleFilterDropdown = useCallback(
+    (key: SortKey, tagArr: string[]) => {
+      setFilter({
+        ...filter,
+        [key]: tagArr,
       });
     },
     [filter]
@@ -180,16 +261,71 @@ export default function CourseSearch(): React.ReactElement {
     [filter]
   );
 
+  // function doesFilterMatchSection(
+  //   section: Section,
+  //   filter: SortFilter
+  // ): boolean {
+  //   return Object.entries(filter).every(([key, tags]) => {
+  //     if (tags.length === 0) return true;
+  //     if (!isSortKey(key)) return true;
+
+  //     const sortValue = section[key];
+  //     if (sortValue == null) return false;
+
+  //     return tags.includes(sortValue);
+  //   });
+  // }
+
   const handleAddCourse = useCallback(
     (course: CourseBean) => {
-      // handler logic here
-      // need to populate the first and third column over here
-      setSelectedCourse([...selectedCourse, course]);
+      if (desiredCourses.includes(course.id)) return;
+      const toBeExcludedCrns = course.sections
+        .filter((section) => {
+          const timeDecided =
+            section.deliveryMode === ASYNC_DELIVERY_MODE ||
+            (section.meetings.length &&
+              section.meetings.every(
+                (meeting) => meeting.days.length && meeting.period
+              ));
+          const filterMatch = doesFilterMatchSection(section, filter);
+          return !timeDecided || !filterMatch;
+        })
+        .map((section) => section.crn);
+      patchSchedule({
+        desiredCourses: [...desiredCourses, course.id],
+        excludedCrns: [...excludedCrns, ...toBeExcludedCrns],
+        colorMap: { ...colorMap, [course.id]: getRandomColor() },
+      });
+      setKeyword('');
+      inputRef.current?.focus();
     },
-    [selectedCourse]
-    // [filter, desiredCourses, excludedCrns, colorMap,
-    //  inputRef, patchSchedule, selectedCourse]
+    [filter, desiredCourses, excludedCrns, colorMap, inputRef, patchSchedule]
   );
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent<HTMLInputElement>) => {
+      switch (e.key) {
+        case 'Enter': {
+          const course = courses[activeIndex];
+          if (course != null) {
+            handleAddCourse(course);
+          }
+          break;
+        }
+        case 'ArrowDown':
+          setActiveIndex(Math.min(activeIndex + 1, courses.length - 1));
+          break;
+        case 'ArrowUp':
+          setActiveIndex(Math.max(activeIndex - 1, 0));
+          break;
+        default:
+          return;
+      }
+      e.preventDefault();
+    },
+    [courses, handleAddCourse, activeIndex]
+  );
+
   const activeCourse = courses[activeIndex];
 
   return (
@@ -210,6 +346,7 @@ export default function CourseSearch(): React.ReactElement {
                 ref={inputRef}
                 value={keyword}
                 onChange={handleChangeKeyword}
+                onKeyDown={handleKeyDown}
                 className="keyword"
               />
             </div>
@@ -218,6 +355,9 @@ export default function CourseSearch(): React.ReactElement {
             key="creditHours"
             onReset={(key: string): void => handleResetFilter(key)}
             onToggle={(key, tag): void => handleToggleFilter(key, tag)}
+            onToggleDropdown={(key, tags): void =>
+              handleToggleFilterDropdown(key, tags)
+            }
             filter={filter}
           />
           {[
@@ -240,6 +380,8 @@ export default function CourseSearch(): React.ReactElement {
             className={classes(course === activeCourse && 'active')}
             courseId={course.id}
             onAddCourse={(): void => handleAddCourse(course)}
+            onShowInfo={(): void => onShow(course)}
+            courseCardType={COURSE_CARD_TYPES.CourseSearch}
           />
         ))}
       </div>
--- src/components/CourseSearchContainer/index.tsx
+++ src/components/CourseSearchContainer/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import CourseSearch from '../CourseSearch';
+import { Course as CourseBean } from '../../data/beans';
 
 import 'react-virtualized/styles.css';
 import './stylesheet.scss';
 
-export default function CourseSearchContainer(): React.ReactElement {
+export type CourseSearchContainerProps = {
+  onShow: (courseToAdd: CourseBean) => void;
+};
+
+export default function CourseSearchContainer({
+  onShow,
+}: CourseSearchContainerProps): React.ReactElement {
   return (
     <>
       <div className="CourseSearchContainer">
         <h3 className="label">Search for a course</h3>
         <div className="scroller">
-          <CourseSearch />
+          <CourseSearch onShow={onShow} />
         </div>
       </div>
     </>
