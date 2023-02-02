faa8ae2632387153f121055bd52c242d920f1f1e
nathangong <nathangong1234@gmail.com>
2023-02-02 15:35:21 -0500

    Add event to ScheduleContext on submit + errors

--- src/components/EventAdd/index.tsx
+++ src/components/EventAdd/index.tsx
@@ -1,15 +1,12 @@
-import React, {
-    useContext,
-    useEffect,
-    useState,
-  } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 
-import { classes } from '../../utils/misc';
+import { classes, getRandomColor } from '../../utils/misc';
 import { DAYS } from '../../constants';
 import { ScheduleContext } from '../../contexts';
 
 import './stylesheet.scss';
 import Button from '../Button';
+import { castDraft } from 'immer';
 
 export type EventAddProps = {
   className?: string;
@@ -18,119 +15,162 @@ export type EventAddProps = {
 export default function EventAdd({
   className,
 }: EventAddProps): React.ReactElement {
-
-  const [{events, colorMap}, { patchSchedule }] = useContext(ScheduleContext);
-  const [eventName, setEventName] = useState("");
-  const [selectedTags, setSelectedTags] = useState<Array<string>>([]);
-  const [start, setStart] = useState("");
-  const [end, setEnd] = useState("");
+  const [{ events, colorMap }, { patchSchedule }] = useContext(ScheduleContext);
+  const [eventName, setEventName] = useState('');
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [start, setStart] = useState<number>(-1);
+  const [end, setEnd] = useState<number>(-1);
   const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    if (eventName.length > 0 && selectedTags.length > 0 && start.length > 0 && end.length > 0) {
+    if (
+      eventName.length > 0 &&
+      selectedTags.length > 0 &&
+      start !== -1 &&
+      end !== -1 &&
+      !error
+    ) {
       setSubmitDisabled(false);
     } else {
       setSubmitDisabled(true);
     }
-  }, [eventName, selectedTags, start, end])
+  }, [eventName, selectedTags, start, end]);
 
-  // function onSubmit() {
-  //   const startSplit = start.split(":").map(str => Number(str));
-  //   const startNum = startSplit[0]! * 60 + startSplit[1]!;
+  function parseTime(time: string): number {
+    const split = time.split(':').map((str) => Number(str));
+    return split[0]! * 60 + split[1]!;
+  }
 
-  //   const endSplit = end.split(":").map(str => Number(str));
-  //   const endNum = endSplit[0]! * 60 + endSplit[1]!;
+  function handleStartChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newStart = parseTime(event.target.value);
 
-    // const event = {
-    //   id: 0,
-      
-    // };
+    setError('');
+    setStart(newStart);
 
-    // patchSchedule({
-    //   events: [...events, event]
-    // })
-  // }
+    if (end !== -1 && end <= newStart) {
+      setError('Start time must be before end time.');
+    }
+  }
+
+  function handleEndChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newEnd = parseTime(event.target.value);
+
+    setError('');
+    setEnd(newEnd);
+
+    if (start !== -1 && newEnd <= start) {
+      setError('Start time must be before end time.');
+    }
+  }
+
+  const onSubmit = useCallback((): void => {
+    const id = new Date().getTime().toString();
+    const event = {
+      id,
+      name: eventName,
+      period: {
+        start,
+        end,
+      },
+      days: selectedTags,
+    };
+
+    patchSchedule({
+      events: [...castDraft(events), event],
+      colorMap: { ...colorMap, [id]: getRandomColor() },
+    });
+  }, [eventName, start, end, selectedTags, events]);
 
   return (
     <div className={classes('EventAdd', className)}>
-      <div className="add">
+      <form className="add">
         <table>
-          <tr>
-            <td>
-              <div className={classes("label", eventName.length > 0 && 'active')}>
-                Name
-              </div>
-            </td>
-            <td className="input">
-              <input
-                type="text"
-                value={eventName}
-                onChange={(event): void => setEventName(event.target.value)}
-                placeholder="Event Name"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className={classes("label", selectedTags.length > 0 && 'active')}>
-                Days
-              </div>
-            </td>
-            <td className="tag-container">
-              {DAYS.map((tag) => (
+          <tbody>
+            <tr>
+              <td>
                 <div
-                  key={tag}
-                  className={classes('tag', selectedTags.includes(tag) && 'active')}
-                  onClick={(): void => {
-                    if (selectedTags.includes(tag)) {
-                      setSelectedTags(selectedTags.filter(x => x !== tag));
-                    } else {
-                      setSelectedTags([...selectedTags, tag]);
-                    }
-                  }}
+                  className={classes('label', eventName.length > 0 && 'active')}
                 >
-                  {tag}
+                  Name
                 </div>
-              ))}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className={classes("label", start !== "" && 'active')}>
-                Start
-              </div>
-            </td>
-            <td className="input">
-              <input
-                type="time"
-                value={start}
-                onChange={(event): void => {setStart(event.target.value)}}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className={classes("label", end !== "" && 'active')}>
-                End
-              </div>
-            </td>
-            <td className="input">
-              <input
-                type="time"
-                value={end}
-                onChange={(event): void => setEnd(event.target.value)}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={2} className="submit">
-              <Button className="button" disabled={submitDisabled} onClick={() => {/* todo */}}>
-                Add
-              </Button>
-            </td>
-          </tr>
+              </td>
+              <td className="input">
+                <input
+                  type="text"
+                  value={eventName}
+                  onChange={(event): void => setEventName(event.target.value)}
+                  placeholder="Event Name"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div
+                  className={classes(
+                    'label',
+                    selectedTags.length > 0 && 'active'
+                  )}
+                >
+                  Days
+                </div>
+              </td>
+              <td className="tag-container">
+                {DAYS.map((tag) => (
+                  <div
+                    key={tag}
+                    className={classes(
+                      'tag',
+                      selectedTags.includes(tag) && 'active'
+                    )}
+                    onClick={(): void => {
+                      if (selectedTags.includes(tag)) {
+                        setSelectedTags(selectedTags.filter((x) => x !== tag));
+                      } else {
+                        setSelectedTags([...selectedTags, tag]);
+                      }
+                    }}
+                  >
+                    {tag}
+                  </div>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className={classes('label', start !== -1 && 'active')}>
+                  Start
+                </div>
+              </td>
+              <td className="input">
+                <input type="time" onChange={handleStartChange} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div className={classes('label', end !== -1 && 'active')}>
+                  End
+                </div>
+              </td>
+              <td className="input">
+                <input type="time" onChange={handleEndChange} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} className="submit">
+                <Button
+                  className="button"
+                  disabled={submitDisabled}
+                  onClick={onSubmit}
+                >
+                  Add
+                </Button>
+                {error && <div className="error">{error}</div>}
+              </td>
+            </tr>
+          </tbody>
         </table>
-      </div>
+      </form>
     </div>
   );
 }
