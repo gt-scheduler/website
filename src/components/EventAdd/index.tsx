b1498367e994f435de1abce20db2acda4fc88fb1
nathangong <nathangong1234@gmail.com>
2023-02-02 17:58:11 -0500

    Fix import grouping

--- src/components/EventAdd/index.tsx
+++ src/components/EventAdd/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
-
 import { castDraft } from 'immer';
+
 import Button from '../Button';
 import { classes, getRandomColor } from '../../utils/misc';
 import { DAYS } from '../../constants';
