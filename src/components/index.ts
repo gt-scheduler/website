a6a84e61163c2acadd10dfffcbfb047f70661995
Sophia Lin <52588981+sophiazlin@users.noreply.github.com>
2023-02-23 05:07:52 -0500

    Display custom blocks (#165)
    ### Summary
    
    Resolves #153 
    
    Adds custom event blocks to calendar
    
    ### Checklist
    #### UI Requirements
    - [x] The block is placed at the right date and time on the calendar.
    - [x] The block has a minimum height, so even when the event's duration
    is too short, users can at least see the name of the event
    ([Figma](https://www.figma.com/file/CkystexX4qQO9OnZRxktXa/GT-Scheduler-%2F-Spr21?node-id=3207%3A2125&t=8EcWnMFd70puiz4A-0))
    - [x] Name of the event should have a character limit when displayed on
    `Calendar`. Full name is shown in the popup instead.
    - [x] Mobile view works.
    
    #### Functional Requirements
    - [x] Popup appears when hovered over and is focused on click.
    
    ### How to Test
    Add events of varying sizes (10 min, 20 min, 30 min) and varying name
    lengths.
    
    Notes: 
    - There is probably some extraneous code I haven't gotten rid of or
    property edited from TimeBlock.
    - Minimum time block height is set to 15 min.
    - Minimum time block length to see time info is set to 30 min.
    
    ---------
    
    Co-authored-by: Nghi Ho <38119460+nhatnghiho@users.noreply.github.com>
    Co-authored-by: Hailey Ho <haileyho@BMESNEL-MBP14-A-2.local>

--- src/components/index.ts
+++ src/components/index.ts
@@ -22,6 +22,8 @@ export { default as Section } from './Section';
 export { default as Select } from './Select';
 export { default as Tab } from './Tab';
 export { default as TimeBlocks } from './TimeBlocks';
+export { default as SectionBlocks } from './SectionBlocks';
+export { default as EventBlocks } from './EventBlocks';
 export { default as Attribution } from './Attribution';
 export { default as Event } from './Event';
 export { default as CourseNavMenu } from './CourseNavMenu';
