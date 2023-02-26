189acf55ac759cb13f35eb36ca8aff5d45ce0e26
Sophia <sophialin00000@gmail.com>
2023-02-26 04:05:16 -0500

    Progress commit

--- src/components/ComparisonContainer/index.tsx
+++ src/components/ComparisonContainer/index.tsx
@@ -1,10 +1,53 @@
-import React, { useState } from 'react';
-import { CombinationContainer } from '..';
+import React, { useState, useContext, useCallback } from 'react';
 import { classes } from '../../utils/misc';
+import { ScheduleContext } from '../../contexts';
+import { faPencil, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button, { ButtonProps } from '../Button';
 import './stylesheet.scss';
+
+export type SharedSchedule = {
+  name: string;
+  schedules: {
+    name: string;
+    color: string;
+  }[];
+};
 
 export default function ComparisonContainer(): React.ReactElement {
   const [compare, setCompare] = useState(false);
+  const [{ allVersionNames }, { deleteVersion, renameVersion }] =
+    useContext(ScheduleContext);
+
+  const handleEditSchedule = useCallback(() => {
+    console.log('edit friend schedule');
+  }, []);
+
+  const handleRemoveSchedule = useCallback(() => {
+    console.log('remove friend schedule');
+  }, []);
+
+  const sharedSchedules: SharedSchedule[] = [
+    {
+      name: 'John Smith',
+      schedules: [
+        { name: 'Main', color: '#760000' },
+        { name: 'Backup', color: '#760076' },
+      ],
+    },
+    {
+      name: 'friend2@gatech.edu',
+      schedules: [
+        { name: 'Primary', color: '#007600' },
+        { name: 'New Name', color: '#000076' },
+        { name: 'Alternative', color: '#007676' },
+      ],
+    },
+    {
+      name: 'friend3@yahoo.com',
+      schedules: [{ name: 'Preferred', color: '#562738' }],
+    },
+  ];
 
   return (
     <div className="comparison-container">
@@ -24,13 +67,77 @@ export default function ComparisonContainer(): React.ReactElement {
         <div className="comparison-content">
           <div className="my-schedules">
             <p className="content-title">My Schedules</p>
+            {allVersionNames.map((version, i) => {
+              return (
+                <div className="checkbox-container">
+                  <div className="checkbox" />
+                  {/* style={selected ? {background-color: {versionBgColor}} : {}} */}
+                  <p>{version.name}</p>
+                  <Button
+                    className="icon"
+                    onClick={handleEditSchedule}
+                    key={`${i}-edit`}
+                  >
+                    <FontAwesomeIcon icon={faPencil} size="xs" />
+                  </Button>
+                  <Button
+                    className="icon"
+                    onClick={handleRemoveSchedule}
+                    key={`${i}-delete`}
+                  >
+                    <FontAwesomeIcon icon={faCircleXmark} size="xs" />
+                  </Button>
+                </div>
+              );
+            })}
           </div>
           <div className="shared-schedules">
             <p className="content-title">Shared with me</p>
+            {sharedSchedules.map((friend) => {
+              return (
+                <div className="friend">
+                  <p>{friend.name}</p>
+                  {friend.schedules.map((schedule, i) => {
+                    return (
+                      <div className="checkbox-container">
+                        <div className="checkbox" />
+                        {/* style={selected ? {background-color: {versionBgColor}} : {}} */}
+                        <p>{schedule.name}</p>
+                        <Button
+                          className="icon"
+                          onClick={handleEditSchedule}
+                          key={`${i}-edit`}
+                        >
+                          <FontAwesomeIcon icon={faPencil} size="xs" />
+                        </Button>
+                        {friend.schedules.length >= 2 && (
+                          <Button
+                            className="icon"
+                            onClick={handleRemoveSchedule}
+                            key={`${i}-delete`}
+                          >
+                            <FontAwesomeIcon icon={faCircleXmark} size="xs" />
+                          </Button>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
           </div>
+          <div
+            className={classes('comparison-overlay', 'left', compare && 'open')}
+          />
+          <div
+            className={classes(
+              'comparison-overlay',
+              'right',
+              !compare && 'open'
+            )}
+          />
         </div>
       </div>
-      <div className={classes('comparison-overlay', compare && 'open')} />
     </div>
   );
 }
--- src/components/ComparisonPanel/index.tsx
+++ src/components/ComparisonPanel/index.tsx
@@ -1,19 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useId } from 'react';
 import { CombinationContainer, ComparisonContainer } from '..';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 import { classes } from '../../utils/misc';
 import './stylesheet.scss';
 
 export default function ComparisonPanel(): React.ReactElement {
   const [expanded, setExpanded] = useState(true);
+  const [hover, setHover] = useState(false);
+  const tooltipId = useId();
 
   return (
     <div className="comparison-panel">
-      <div className="drawer" onClick={(): void => setExpanded(!expanded)}>
+      <div
+        className="drawer"
+        onClick={(): void => {
+          setExpanded(!expanded);
+          setHover(false);
+        }}
+        onMouseEnter={(): void => setHover(true)}
+        onMouseLeave={(): void => setHover(false)}
+        id={tooltipId}
+      >
         <div className="drawer-line" />
         <div className="icon">
           <div className={classes('arrow', expanded && 'right')} />
         </div>
         <div className="drawer-line" />
+        <ReactTooltip
+          anchorId={tooltipId}
+          className="tooltip"
+          variant="dark"
+          clickable
+          isOpen={hover}
+          setIsOpen={setHover}
+          delayShow={20}
+          delayHide={100}
+          float
+          // key={deviceHasHover ? 0 : 1}
+          // events={deviceHasHover ? ['hover'] : []}
+        >
+          <p>{expanded ? 'Collapse' : 'Expand for More Options'}</p>
+        </ReactTooltip>
       </div>
       <div className={classes('panel', !expanded && 'closed')}>
         <ComparisonContainer />
