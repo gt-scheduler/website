2aae2db913b5766090bd62dc86a9eff76d1ed080
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-03-04 18:38:52 -0500

    lint

--- src/data/hooks/useVersionActions.ts
+++ src/data/hooks/useVersionActions.ts
@@ -180,7 +180,7 @@ export default function useVersionActions({
         setVersion(newId);
       });
     },
-    [updateTermScheduleData, addNewVersion]
+    [updateTermScheduleData]
   );
 
   return { addNewVersion, deleteVersion, renameVersion, cloneVersion };
