466d8e10fccc8e1becb58fb869278b68bc3bb5a0
Sophia <sophialin00000@gmail.com>
2023-03-04 19:12:51 -0500

    Migration page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -12,6 +12,7 @@ import ErrorHeader from '../ErrorHeader';
 import { AppNavigation } from './navigation';
 import AppDataLoader from '../AppDataLoader';
 import { AppSkeleton, SkeletonContent, AppContent } from './content';
+import Maintenance from './maintenance';
 import useThemeFromStorage from '../../data/hooks/useThemeFromStorage';
 import { DESKTOP_BREAKPOINT } from '../../constants';
 import useScreenWidth from '../../hooks/useScreenWidth';
@@ -28,49 +29,63 @@ export default function App(): React.ReactElement {
   // Add the current theme as a class on the body element
   useBodyClass(themeContextValue[0]);
 
+  const date = new Date();
+  const websiteDown =
+    date.getFullYear() === 2023 &&
+    date.getMonth() === 2 &&
+    (date.getDate() === 15 || date.getDate() === 16);
+
   return (
     <ThemeContext.Provider value={themeContextValue}>
       <AppCSSRoot>
         <TooltipProvider>
-          <ErrorBoundary
-            fallback={(error, errorInfo): React.ReactElement => (
-              <AppSkeleton>
-                <SkeletonContent>
-                  <ErrorHeader />
-                  <ErrorDisplay
-                    errorDetails={
-                      <ReactErrorDetails error={error} errorInfo={errorInfo} />
-                    }
-                  >
-                    <div>
-                      There was en error somewhere in the core application logic
-                      and it can&apos;t continue.
-                    </div>
-                    <div>
-                      Try refreshing the page to see if it fixes the issue.
-                    </div>
-                  </ErrorDisplay>
-                </SkeletonContent>
-              </AppSkeleton>
-            )}
-          >
-            <AppNavigation>
-              {/* AppDataLoader is in charge of ensuring that there are valid values
-                for the Terms and Term contexts before rendering its children.
-                If any data is still loading,
-                then it displays an "app skeleton" with a spinner.
-                If there was an error while loading
-                then it displays an error screen. */}
-              <AppDataLoader>
-                <AppContent />
-              </AppDataLoader>
-            </AppNavigation>
-            <Feedback />
-
+          {!websiteDown && (
+            <ErrorBoundary
+              fallback={(error, errorInfo): React.ReactElement => (
+                <AppSkeleton>
+                  <SkeletonContent>
+                    <ErrorHeader />
+                    <ErrorDisplay
+                      errorDetails={
+                        <ReactErrorDetails
+                          error={error}
+                          errorInfo={errorInfo}
+                        />
+                      }
+                    >
+                      <div>
+                        There was en error somewhere in the core application
+                        logic and it can&apos;t continue.
+                      </div>
+                      <div>
+                        Try refreshing the page to see if it fixes the issue.
+                      </div>
+                    </ErrorDisplay>
+                  </SkeletonContent>
+                </AppSkeleton>
+              )}
+            >
+              <AppNavigation>
+                {/* AppDataLoader is in charge of ensuring that there are valid values
+                  for the Terms and Term contexts before rendering its children.
+                  If any data is still loading,
+                  then it displays an "app skeleton" with a spinner.
+                  If there was an error while loading
+                  then it displays an error screen. */}
+                <AppDataLoader>
+                  <AppContent />
+                </AppDataLoader>
+              </AppNavigation>
+              <Feedback />
             {/* Display a popup when first visiting the site */}
             {/* Include <InformationModal /> here */}
             <MaintenanceModal />
           </ErrorBoundary>
+              {/* Display a popup when first visiting the site */}
+              {/* Include <InformationModal /> here */}
+            </ErrorBoundary>
+          )}
+          {websiteDown && <Maintenance />}
         </TooltipProvider>
       </AppCSSRoot>
     </ThemeContext.Provider>
