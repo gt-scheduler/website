95255e18dc12c0c0c87e7641e3ec9257d10c0580
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-03-28 01:07:59 -0400

    working drag and drop

--- src/components/EventBlocks/index.tsx
+++ src/components/EventBlocks/index.tsx
@@ -1,11 +1,12 @@
-import React, { ReactElement, useState } from 'react';
-import { Immutable } from 'immer';
+import React, { ReactElement, useState, useContext, useRef } from 'react';
+import { Immutable, castDraft } from 'immer';
 
 import { daysToString, periodToString } from '../../utils/misc';
 import { TimeBlocks } from '..';
 import { Period, Event } from '../../types';
 import { TimeBlockPosition, SizeInfo } from '../TimeBlocks';
 import { CLOSE, OPEN } from '../../constants';
+import { ThemeContext, ScheduleContext } from '../../contexts';
 
 import './stylesheet.scss';
 
@@ -48,6 +49,15 @@ export default function EventBlocks({
   timesRef,
 }: EventBlocksProps): React.ReactElement | null {
   const [tempStart, setTempStart] = useState<number>(event.period.start);
+  const [tempDays, setTempDays] = useState<string[]>([...event.days]);
+
+  const tempStartRef = useRef<number>(event.period.start);
+  const tempDaysRef = useRef<string[]>([...event.days]);
+
+  const [dragging, setDragging] = useState<boolean>(false);
+  const [theme] = useContext(ThemeContext);
+
+  const [{ events }, { patchSchedule }] = useContext(ScheduleContext);
 
   const handleMouseDown = (
     e: React.MouseEvent,
@@ -56,19 +66,45 @@ export default function EventBlocks({
     const cloneMeeting = ref.current!.cloneNode(true) as HTMLDivElement;
     cloneMeeting.classList.add('meeting--clone');
     cloneMeeting.id = 'meeting--clone';
+
+    setDragging(true);
+
+    ref.current?.classList.remove('light-content', 'dark-content');
+    ref.current?.classList.add(`${theme}-content`);
     ref.current?.parentNode?.appendChild(cloneMeeting);
     ref.current?.classList.add('meeting--dragging');
+
     // eslint-disable-next-line
     const mouseMoveHandler = (e_: any): void => handleMouseMove(e_, ref);
+
     const handleMouseUp = (
       e_: React.MouseEvent,
       ref_: React.RefObject<HTMLDivElement>
     ): void => {
       ref_.current?.classList.remove('meeting--dragging');
       cloneMeeting.remove();
+      setDragging(false);
+
+      const newEvents = castDraft(events).map((existingEvent: Event) => {
+        if (existingEvent.id === event.id) {
+          return {
+            ...existingEvent,
+            period: {
+              start: tempStartRef.current,
+              end: tempStartRef.current + event.period.end - event.period.start,
+            },
+            days: tempDaysRef.current,
+          };
+        }
+        return existingEvent;
+      });
+
+      patchSchedule({ events: newEvents });
+
       document.removeEventListener('mousemove', mouseMoveHandler);
       document.removeEventListener('mouseup', documentMouseUp);
     };
+
     // eslint-disable-next-line
     const documentMouseUp = (e_: any): void => handleMouseUp(e_, ref);
 
@@ -100,10 +136,13 @@ export default function EventBlocks({
     if (day && event.days.length === 1) {
       ref.current!.classList.remove('M', 'T', 'W', 'R', 'F');
       ref.current!.classList.add(day);
+      setTempDays([day]);
+      tempDaysRef.current = [day];
     }
-    console.log(ref.current!.classList);
+
     if (start >= OPEN && end <= CLOSE) {
       setTempStart(start);
+      tempStartRef.current = start;
     }
   };
 
@@ -151,12 +190,10 @@ export default function EventBlocks({
       ]}
       capture={capture}
       sizeInfo={sizeInfo}
-      includeDetailsPopover={includeDetailsPopover}
+      includeDetailsPopover={!dragging && includeDetailsPopover}
       includeContent={includeContent}
       canBeTabFocused={canBeTabFocused}
       deviceHasHover={deviceHasHover}
-      selectedMeeting={selectedMeeting}
-      onSelectMeeting={onSelectMeeting}
       handleMouseDown={handleMouseDown}
     />
   );
