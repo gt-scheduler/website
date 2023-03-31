e516b4bea140c0db9eb170695706b17bd5b8de4c
nathangong <nathangong1234@gmail.com>
2023-03-31 02:22:01 -0400

    Create email invite confirmation screen

--- src/components/RouterComponent/index.tsx
+++ src/components/RouterComponent/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import App from '../App';
+import EmailInviteConfirmation from '../EmailInviteConfirmation';
 import InviteBackLink from '../InviteBackLink';
 
 export default function RouterComponent(): React.ReactElement {
@@ -10,6 +11,7 @@ export default function RouterComponent(): React.ReactElement {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/invite/:id" element={<InviteBackLink />} />
+        <Route path="/confirm-invite" element={<EmailInviteConfirmation />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
