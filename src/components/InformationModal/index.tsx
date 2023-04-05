a6a95e1ba73c4f646c3b4ae4bd3f8b04c13e7508
Sophia <sophialin00000@gmail.com>
2023-04-04 21:06:38 -0400

    Modal

--- src/components/InformationModal/index.tsx
+++ src/components/InformationModal/index.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import useLocalStorageState from 'use-local-storage-state';
 
+import { classes } from '../../utils/misc';
+import { DESKTOP_BREAKPOINT } from '../../constants';
 import Modal from '../Modal';
+import './stylesheet.scss';
+
+import useScreenWidth from '../../hooks/useScreenWidth';
 
 // Key to mark when a user has already been shown the information modal.
 // Update this when updating the contents of the modal.
-const MODAL_LOCAL_STORAGE_KEY =
-  '2021-10-27-spr2022-schedule-versions-account-sync';
+const MODAL_LOCAL_STORAGE_KEY = '2023-04-05-spr2023-new-features-announcement';
 
 /**
  * Inner content of the information modal.
@@ -18,84 +22,51 @@ export function InformationModalContent(): React.ReactElement {
   return (
     <>
       <img
-        style={{ width: '175px', margin: '0 auto', display: 'block' }}
+        style={{ width: '150px', margin: '0 auto', display: 'block' }}
         alt="GT Scheduler Logo"
         src="/mascot.png"
       />
-      <h1 style={{ lineHeight: 1 }}>
-        Spring 2022 courses, schedule versions, and account sync
+      <h1
+        style={{
+          lineHeight: 1,
+          fontWeight: 700,
+          fontSize: '28px',
+          margin: '16px auto',
+        }}
+      >
+        New Feature: Recurring Events
       </h1>
-      <h4 style={{ opacity: 0.7 }}>October 27th, 2021</h4>
-      <p>
-        Hey there, yellow jackets! Today marks the release of the Spring 2022
-        course catalog. You can access it in GT Scheduler by selecting{' '}
-        <strong>&ldquo;Spring 2022&rdquo;</strong> from the term drop-down in
-        the top-left.
-      </p>
-      <p>
-        On behalf of the GT Scheduler contributors, we&apos;re also excited to
-        announce two of the most-requested features:
-      </p>
-      <ul>
-        <li style={{ marginBottom: 8 }}>
-          <strong>Schedule versions</strong> - Now, next to the term drop-down
-          in the top-left, there is also a schedule drop-down. Use it to{' '}
-          <strong>create new schedules</strong> and{' '}
-          <strong>switch between existing ones</strong>.
+      <h4 style={{ opacity: 0.7, fontWeight: 700 }}>April 5, 2023</h4>
+      <div className="information-content">
+        <p>
+          Hello <span style={{ color: '#EDA91F' }}>Yellow Jackets!</span> We are
+          excited to announce a new feature for GT Scheduler.
+          <br />
+          <br />
+          Use Recurring Events to block out meetings, work shifts, and any other
+          weekly events you may need to schedule your classes around.
+          <br />
+          <br />
+          Add, edit, and delete events in the Recurring Events tab, next to the
+          Courses tab.
+          <br />
+          <br />
+          Drag and drop (web-only) events on the schedule view to adjust times.
+        </p>
+        <div className="information-images">
           <img
-            style={{
-              width: '100%',
-              height: 'auto',
-              margin: '8px auto',
-              display: 'block',
-            }}
+            className="information-image"
             alt="GT Scheduler Logo"
-            src="/schedule_versions.png"
+            src="/event_blocks.png"
           />
-        </li>
-        <li>
-          <strong>Account sync</strong> - In addition to creating multiple
-          schedules, you can now also{' '}
-          <strong>sync these schedules between devices.</strong> Use the
-          drop-down in the top right to sign in and upload your schedules to the
-          cloud. Currently, the app supports either email/password, Google, or
-          GitHub sign-in options:
+          <div className="information-spacer" />
           <img
-            style={{
-              width: '100%',
-              height: 'auto',
-              margin: '16px auto',
-              display: 'block',
-            }}
+            className="information-image"
             alt="GT Scheduler Logo"
-            src="/account_sync.png"
+            src="/drag_event_blocks.png"
           />
-          Note that signing in is completely optional: the app will still work
-          without an account (your schedules will instead be stored locally on
-          your device).
-        </li>
-      </ul>
-      <p>
-        If you find any bugs in these features or in existing ones, feel free to{' '}
-        <a href="https://github.com/gt-scheduler/website/issues/new/choose">
-          open an issue on the GT Scheduler GitHub
-        </a>{' '}
-        and we will try our best to fix them.
-      </p>
-      <p>
-        A big thank you to everyone that{' '}
-        <a href="https://github.com/gt-scheduler/website/graphs/contributors">
-          contributed
-        </a>{' '}
-        to the project in the past year. GT Scheduler is an open source project,
-        and we hope that we&apos;ve been able to make your scheduling experience
-        better. If you enjoy our work and are interested in contributing, feel
-        free to{' '}
-        <a href="https://github.com/gt-scheduler/website/pulls">
-          open a pull request
-        </a>{' '}
-        with your improvements. Thank you and enjoy!
-      </p>
+        </div>
+      </div>
     </>
   );
 }
@@ -111,6 +82,7 @@ export default function InformationModal(): React.ReactElement {
     defaultValue: false,
     storageSync: true,
   });
+  const mobile = !useScreenWidth(DESKTOP_BREAKPOINT);
 
   useEffect(() => {
     if (!hasSeen) {
@@ -121,6 +93,7 @@ export default function InformationModal(): React.ReactElement {
 
   return (
     <Modal
+      className={classes('InformationModal', mobile && 'mobile')}
       show={show}
       onHide={(): void => setShow(false)}
       buttons={[{ label: 'Got it!', onClick: (): void => setShow(false) }]}
