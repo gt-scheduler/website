83f447eeb145ff7269b50d24061c74a1bccbb046
Nathan Gong <nathangong1234@gmail.com>
2023-10-19 12:46:43 -0500

    Create email invite confirmation screen (#215)
    ### Summary
    
    Resolves #205 
    
    Creates a confirmation screen for email invite, access through
    /confirm-invite
    
    ### Checklist
    
    #### UI Requirements
    ~~- [ ] Information about the invitation (i.e., the author of the
    schedule, its name, and the account that is given access to this
    schedule) is displayed.~~ -- Figma doesn't specify any of this
    information (?)
    
    #### Functional Requirements
    - [x] The screen automatically redirects after 30s.
    - [x] Works for preview sites.
    
    ### How to Test
    Run it locally and navigate to http://localhost:3000/confirm-invite
    
    ---------
    
    Co-authored-by: Samarth Chandna <57265280+samarth52@users.noreply.github.com>
    Co-authored-by: Samarth Chandna <samarthchandna52@gmail.com>

--- src/components/InviteBackLink/index.tsx
+++ src/components/InviteBackLink/index.tsx
@@ -1,34 +1,96 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-// import { CLOUD_FUNCTION_BASE_URL } from '../../constants';
+import { CLOUD_FUNCTION_BASE_URL } from '../../constants';
+import Spinner from '../Spinner';
 
-const handleInvite = async (inviteId: string | undefined): Promise<void> =>
-  // The link should be changed to prod link, or we can choose the link based
-  // on environment
-  axios.post(
-    `http://127.0.0.1:5001/gt-scheduler-web-dev/us-central1/handleFriendInvitation`,
-    { inviteId }
-  );
-// .then((res) => {
-//   console.log(res);
-// })
-// .catch((err) => {
-//   console.error(err);
-// });
+import './stylesheet.scss';
+
+// eslint-disable-next-line no-shadow
+enum LoadingState {
+  LOADING,
+  SUCCESS,
+  ERROR,
+}
+
+const url = `${CLOUD_FUNCTION_BASE_URL}/handleFriendInvitation`;
+
+const handleInvite = async (inviteId: string | undefined): Promise<void> => {
+  const requestData = JSON.stringify({ inviteId });
+  await axios({
+    method: 'POST',
+    url,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: `data=${requestData}`,
+  });
+};
 
 export default function InviteBackLink(): React.ReactElement {
   const navigate = useNavigate();
   const { id } = useParams();
+  const [state, setState] = useState(LoadingState.LOADING);
 
   useEffect(() => {
     if (id && navigate) {
       handleInvite(id)
-        .then(() => navigate('/'))
-        .catch(() => navigate('/'));
+        .then(() => {
+          setState(LoadingState.SUCCESS);
+          setTimeout(() => {
+            navigate('/');
+          }, 5000);
+        })
+        .catch(() => {
+          setState(LoadingState.ERROR);
+          setTimeout(() => {
+            navigate('/');
+          }, 10000);
+        });
     }
   }, [id, navigate]);
 
-  return <div />;
+  if (state === LoadingState.LOADING) {
+    return (
+      <div className="Loading">
+        <Spinner size="normal" style={{ opacity: 0.6 }} />
+        <h4>Loading</h4>
+        <div>friend schedule invite</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="EmailInviteConfirmation">
+      {state === LoadingState.SUCCESS ? (
+        <h1>Congratulations on Adding a New Schedule to your View!</h1>
+      ) : (
+        <h1>We&apos;ve Encountered an Error, Please Try Again</h1>
+      )}
+      <p>You are being redirected to our main site, please wait...</p>
+      <p>
+        If you have not been redirected in 30 seconds, please click the button
+        below
+      </p>
+      <button
+        type="button"
+        className="continue-button"
+        onClick={(): void => {
+          navigate('/');
+        }}
+      >
+        Continue
+      </button>
+
+      <a className="footer" href="https://bitsofgood.org/">
+        <img
+          alt="Bits of Good Logo"
+          height={44}
+          width={243}
+          src="/bitsOfGood.svg"
+        />
+      </a>
+    </div>
+  );
 }
