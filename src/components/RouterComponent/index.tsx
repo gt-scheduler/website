0ab21fe9a934351c94feb2ed8df0c7f8cc750a08
Samarth Chandna <samarthchandna52@gmail.com>
2023-10-21 17:06:19 -0400

    Fixed PR Preview auto redirect

--- src/components/RouterComponent/index.tsx
+++ src/components/RouterComponent/index.tsx
@@ -5,11 +5,18 @@ import App from '../App';
 import InviteBackLink from '../InviteBackLink';
 
 export default function RouterComponent(): React.ReactElement {
+  console.log('BASE_URL', process.env.PUBLIC_URL);
+  console.log('NODE_ENV', process.env.NODE_ENV);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/invite/:id" element={<InviteBackLink />} />
+        <Route path="/pr-preview/:pr/invite/:id" element={<InviteBackLink />} />
+        <Route
+          path="/pr-preview/:pr/*"
+          element={<Navigate to="/pr-preview/:pr/" replace />}
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
