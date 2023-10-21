f54f527f2ec766e819bb304e51f7d3fd89bde439
Samarth Chandna <samarthchandna52@gmail.com>
2023-10-21 19:13:23 -0400

    Updated react router for pr-preview

--- src/components/RouterComponent/index.tsx
+++ src/components/RouterComponent/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
@@ -6,15 +5,16 @@ import App from '../App';
 import InviteBackLink from '../InviteBackLink';
 
 export default function RouterComponent(): React.ReactElement {
-  console.log('BASE_URL', process.env.PUBLIC_URL);
-  console.log('NODE_ENV', process.env.NODE_ENV);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/invite/:id" element={<InviteBackLink />} />
-        <Route path="/pr-preview/:pr/" element={<App />} />
-        <Route path="/pr-preview/:pr/invite/:id" element={<InviteBackLink />} />
+        <Route path="/pr-preview/:pr/">
+          <Route path="/" element={<App />} />
+          <Route path="/invite/:id" element={<InviteBackLink />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
