1f34cc81579c850046b6b692f42df85ffa769e8b
Samarth Chandna <samarthchandna52@gmail.com>
2023-10-21 20:33:52 -0400

    changed BrowserRouter to HashRouter

--- src/components/RouterComponent/index.tsx
+++ src/components/RouterComponent/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 
 import App from '../App';
 import InviteBackLink from '../InviteBackLink';
 
 export default function RouterComponent(): React.ReactElement {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/invite/:id" element={<InviteBackLink />} />
@@ -16,6 +16,6 @@ export default function RouterComponent(): React.ReactElement {
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
