a8a3f09cf0e1292c0a0ea09b0d7be51ec078cbc6
Joanna Cheng <joannacheng21@outlook.com>
2024-01-29 16:05:07 -0500

    add fade-in/out

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './stylesheet.scss';
 import { classes } from '../../utils/misc';
-import { faWarning } from '@fortawesome/free-solid-svg-icons';
+import { faWarning, faClose } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
@@ -23,20 +23,30 @@ export default function Toast({
 }: ToastProps): React.ReactElement {
   function notify(): void {
     const x = document.getElementById('toast');
-    x!.className = 'show';
-    setTimeout(function () {
-      x!.className = x!.className.replace('show', '');
-    }, 3000);
+    x!.style.visibility = 'visible';
+    x!.style.animation = 'fadein 0.5s';
   }
 
   return (
     <div>
       <button type="button" onClick={notify}>
-        Show Snackbar
+        Show Toast
       </button>
       <div id="toast" style={{ backgroundColor: color }}>
         <FontAwesomeIcon fixedWidth icon={icon} className="toast-icon" />
-        {message}
+        <div className="toast-message">{message}</div>
+        <FontAwesomeIcon
+          fixedWidth
+          icon={faClose}
+          className="toast-close-icon"
+          onClick={(): void => {
+            const x = document.getElementById('toast');
+            x!.style.animation = 'fadeout 0.5s';
+            setTimeout(function () {
+              x!.style.visibility = 'hidden';
+            }, 500);
+          }}
+        />
       </div>
       <div className={classes('toast', className)} />
     </div>
