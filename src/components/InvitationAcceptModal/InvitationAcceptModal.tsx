26efa8133a5c68fa5aa23ca78e9227c9a171096f
Yatharth Bhargava <yatharthbhargava@gmail.com>
2024-02-26 23:21:46 -0500

    nit

--- src/components/InvitationAcceptModal/InvitationAcceptModal.tsx
+++ src/components/InvitationAcceptModal/InvitationAcceptModal.tsx
@@ -1,4 +1,7 @@
 import Modal from '../Modal';
+import Button from '../Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 import React, { useState } from 'react';
 
@@ -7,17 +10,34 @@ import './stylesheet.scss';
 export default function InvitationAcceptModal(): React.ReactElement {
   const [modalOpen, setModalOpen] = useState<boolean>(true);
 
+  const onHide = (): void => {
+    setModalOpen(!modalOpen);
+  };
+
   return (
-    <Modal
-      show={modalOpen}
-      onHide={(): void => {
-        setModalOpen(!modalOpen);
-      }}
-    >
+    <Modal show={modalOpen} onHide={onHide} width={700}>
+      <Button className="remove-close-button" onClick={onHide}>
+        <FontAwesomeIcon icon={faXmark} size="xl" />
+      </Button>
       <div className="invitation-accept-modal-content">
-        <h2 className="heading">
+        <div className="heading">
           You have successfully added a new schedule to your view!
-        </h2>
+        </div>
+        <div className="sub-heading">
+          You will now be able to see winniezhang@gmail.com‘s schedule!
+        </div>
+        <img src="/scheduled.png" alt="ok" className="modal-image" />
+        <div className="modal-bottom">
+          <div>Would you like to share your schedule back?</div>
+          <div className="button-row">
+            <button type="submit" className="no-button">
+              No
+            </button>
+            <button type="submit" className="share-button">
+              Share Back
+            </button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
