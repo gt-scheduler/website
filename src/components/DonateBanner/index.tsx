c8810e4fda421109526879815e6e1145ff349102
Nathan Papa <nathanpapa35@gmail.com>
2024-03-30 17:17:08 -0400

    Initial banner functionality

--- src/components/DonateBanner/index.tsx
+++ src/components/DonateBanner/index.tsx
@@ -2,25 +2,23 @@ import React, { useEffect, useState } from 'react';
 import useLocalStorageState from 'use-local-storage-state';
 
 import { classes } from '../../utils/misc';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import Button from '../Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DESKTOP_BREAKPOINT } from '../../constants';
 import useScreenWidth from '../../hooks/useScreenWidth';
+import usePrevious from '../../hooks/usePrevious';
 
 import './stylesheet.scss';
 
-// Key to mark when a user has already been shown the information modal.
-// Update this when updating the contents of the modal.
 const BANNER_LOCAL_STORAGE_KEY = '2023-04-05-spr2024-donate-banner';
-const OUTDATED_LOCAL_STORAGE_KEY = [
-  '2023-04-05-spr2024-donate-banner',
-  '2021-10-27-spr2022-schedule-versions-account-sync',
-  '2023-03-05-spr2023-oscar-migration',
-];
+// const OUTDATED_LOCAL_STORAGE_KEY = [
+//   '2023-04-05-spr2024-donate-banner',
+//   '2023-04-05-spr2023-new-features-announcement',
+//   '2021-10-27-spr2022-schedule-versions-account-sync',
+//   '2023-03-05-spr2023-oscar-migration',
+// ];
 
-/**
- * Component that shows the information modal
- * upon the user's first visit to the site
- * when they haven't seen this version of the information modal before.
- */
 export default function DonateBanner(): React.ReactElement {
   const [show, setShow] = useState(false);
   const [hasSeen, setHasSeen] = useLocalStorageState(BANNER_LOCAL_STORAGE_KEY, {
@@ -28,10 +26,6 @@ export default function DonateBanner(): React.ReactElement {
     storageSync: true,
   });
   const mobile = !useScreenWidth(DESKTOP_BREAKPOINT);
-
-  OUTDATED_LOCAL_STORAGE_KEY.forEach((key) =>
-    window.localStorage.removeItem(key)
-  );
 
   useEffect(() => {
     if (!hasSeen) {
@@ -42,7 +36,32 @@ export default function DonateBanner(): React.ReactElement {
 
   return (
     <div>
-      <p>Donate now!</p>
+      {show ? (
+        <div className="banner">
+          <div>&nbsp;</div>
+          <span>
+            {!mobile
+              ? 'Help keep GT Scheduler and its amazing features running!'
+              : 'Help keep GT Scheduler running by'}
+            <Button
+              className="donateButton"
+              href="https://donorbox.org/gt-scheduler"
+            >
+              <b>{!mobile ? 'Donate today.' : 'donating today.'}</b>
+            </Button>
+          </span>
+          <Button
+            className="close-button"
+            onClick={(): void => {
+              setShow(false);
+            }}
+          >
+            <FontAwesomeIcon fixedWidth icon={faX} size="lg" />
+          </Button>
+        </div>
+      ) : (
+        <div />
+      )}
     </div>
   );
 }
