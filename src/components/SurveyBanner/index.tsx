0d79ee84866b68f3810449e84927fdef9b63f45c
Uma Anand <uma2005anand@gmail.com>
2025-09-25 16:11:57 -0400

    update survey banner link and token

--- src/components/SurveyBanner/index.tsx
+++ src/components/SurveyBanner/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import Banner from '../Banner';
 
-const BANNER_LOCAL_STORAGE_KEY = '2025-08-19-fall2025-survey-banner';
+const BANNER_LOCAL_STORAGE_KEY =
+  '2025-09-25-fall2025-course-details-survey-banner';
 const SURVEY_LINK =
-  'https://gatech.co1.qualtrics.com/jfe/form/SV_d7tz8mwG0NP2CF0';
+  'https://gatech.co1.qualtrics.com/jfe/form/SV_0wwncvjPzzI8Q9E';
 
 function Content(): React.ReactElement {
   return (
     <span>
-      Help shape the future of GT Scheduler!
+      Help us create our new course page!
       <a
         className="bannerButton"
         href={SURVEY_LINK}
