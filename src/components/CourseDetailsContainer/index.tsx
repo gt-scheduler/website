1f7a95607b47f787a79273cfcc97d90710c8dc67
Uma Anand <uma2005anand@gmail.com>
2025-11-18 16:09:32 -0500

    fix empty coruse details page

--- src/components/CourseDetailsContainer/index.tsx
+++ src/components/CourseDetailsContainer/index.tsx
@@ -7,12 +7,13 @@ import './stylesheet.scss';
 import CourseInfoCard from '../CourseInfoCard';
 
 export default function CourseDetailsContainer(): React.ReactElement {
-  const [{ oscar, desiredCourses }] = useContext(ScheduleContext);
+  const [{ desiredCourses }] = useContext(ScheduleContext);
 
   if (desiredCourses.length === 0) {
     return (
       <div className="empty-container">
         {/* TODO: insert image here */}
+        <img src="/empty_course_details.svg" alt="Empty Course Details" />
         <span className="empty-title">Course Details</span>
         <span className="empty-description">Add courses to view details</span>
       </div>
